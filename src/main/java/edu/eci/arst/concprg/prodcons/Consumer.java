39d1d21d54d20f01106c677947b1da38c8aade7f
unknown <laura.garcia-l@labinfo.is.escuelaing.edu.co>
2022-02-02 09:27:37 -0500

    ARSW-03: Part1-3 done

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -32,7 +32,7 @@ public class Consumer extends Thread{
             }
 
             try {
-                Thread.sleep(500);
+                Thread.sleep(5000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
             }
--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -31,16 +31,19 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
-
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+            if(queue.size() + 1 <= this.stockLimit){
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                queue.add(dataSeed);
             }
+
+
+            
+//            try {
+//                Thread.sleep(50);
+//            } catch (InterruptedException ex) {
+//                Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
+//            }
 
         }
     }
